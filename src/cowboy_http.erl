a9d4256de526a4a6c7a52ebdf45385d2f9b635dc
Louis Philippe Gauthiers <lpgauth@h052.bloomdigital.com>
2013-10-18 16:37:52 -0400

    Use Quoted.erl for url encode/decode

--- src/cowboy_http.erl
+++ src/cowboy_http.erl
@@ -1049,88 +1049,24 @@ cookie_to_iodata(Name, Value, Opts) ->
 		MaxAgeBin, DomainBin, PathBin, SecureBin, HttpOnlyBin].
 
 %% @doc Decode a URL encoded binary.
-%% @equiv urldecode(Bin, crash)
 -spec urldecode(binary()) -> binary().
 urldecode(Bin) when is_binary(Bin) ->
-	urldecode(Bin, <<>>, crash).
+	quoted:from_url(Bin).
 
 %% @doc Decode a URL encoded binary.
-%% The second argument specifies how to handle percent characters that are not
-%% followed by two valid hex characters. Use `skip' to ignore such errors,
-%% if `crash' is used the function will fail with the reason `badarg'.
--spec urldecode(binary(), crash | skip) -> binary().
-urldecode(Bin, OnError) when is_binary(Bin) ->
-	urldecode(Bin, <<>>, OnError).
-
--spec urldecode(binary(), binary(), crash | skip) -> binary().
-urldecode(<<$%, H, L, Rest/binary>>, Acc, OnError) ->
-	G = unhex(H),
-	M = unhex(L),
-	if	G =:= error; M =:= error ->
-		case OnError of skip -> ok; crash -> erlang:error(badarg) end,
-		urldecode(<<H, L, Rest/binary>>, <<Acc/binary, $%>>, OnError);
-		true ->
-		urldecode(Rest, <<Acc/binary, (G bsl 4 bor M)>>, OnError)
-	end;
-urldecode(<<$%, Rest/binary>>, Acc, OnError) ->
-	case OnError of skip -> ok; crash -> erlang:error(badarg) end,
-	urldecode(Rest, <<Acc/binary, $%>>, OnError);
-urldecode(<<$+, Rest/binary>>, Acc, OnError) ->
-	urldecode(Rest, <<Acc/binary, $ >>, OnError);
-urldecode(<<C, Rest/binary>>, Acc, OnError) ->
-	urldecode(Rest, <<Acc/binary, C>>, OnError);
-urldecode(<<>>, Acc, _OnError) ->
-	Acc.
-
--spec unhex(byte()) -> byte() | error.
-unhex(C) when C >= $0, C =< $9 -> C - $0;
-unhex(C) when C >= $A, C =< $F -> C - $A + 10;
-unhex(C) when C >= $a, C =< $f -> C - $a + 10;
-unhex(_) -> error.
-
+-spec urldecode(binary(), term()) -> binary().
+urldecode(Bin, Opts) when is_binary(Bin) ->
+	quoted:from_url(Bin, Opts).
 
 %% @doc URL encode a string binary.
-%% @equiv urlencode(Bin, [])
 -spec urlencode(binary()) -> binary().
 urlencode(Bin) ->
-	urlencode(Bin, []).
+	quoted:to_url(Bin).
 
 %% @doc URL encode a string binary.
-%% The `noplus' option disables the default behaviour of quoting space
-%% characters, `\s', as `+'. The `upper' option overrides the default behaviour
-%% of writing hex numbers using lowecase letters to using uppercase letters
-%% instead.
--spec urlencode(binary(), [noplus|upper]) -> binary().
+-spec urlencode(binary(), term()) -> binary().
 urlencode(Bin, Opts) ->
-	Plus = not lists:member(noplus, Opts),
-	Upper = lists:member(upper, Opts),
-	urlencode(Bin, <<>>, Plus, Upper).
-
--spec urlencode(binary(), binary(), boolean(), boolean()) -> binary().
-urlencode(<<C, Rest/binary>>, Acc, P=Plus, U=Upper) ->
-	if	C >= $0, C =< $9 -> urlencode(Rest, <<Acc/binary, C>>, P, U);
-		C >= $A, C =< $Z -> urlencode(Rest, <<Acc/binary, C>>, P, U);
-		C >= $a, C =< $z -> urlencode(Rest, <<Acc/binary, C>>, P, U);
-		C =:= $.; C =:= $-; C =:= $~; C =:= $_ ->
-		urlencode(Rest, <<Acc/binary, C>>, P, U);
-		C =:= $ , Plus ->
-		urlencode(Rest, <<Acc/binary, $+>>, P, U);
-		true ->
-		H = C band 16#F0 bsr 4, L = C band 16#0F,
-		H1 = if Upper -> tohexu(H); true -> tohexl(H) end,
-		L1 = if Upper -> tohexu(L); true -> tohexl(L) end,
-		urlencode(Rest, <<Acc/binary, $%, H1, L1>>, P, U)
-	end;
-urlencode(<<>>, Acc, _Plus, _Upper) ->
-	Acc.
-
--spec tohexu(byte()) -> byte().
-tohexu(C) when C < 10 -> $0 + C;
-tohexu(C) when C < 17 -> $A + C - 10.
-
--spec tohexl(byte()) -> byte().
-tohexl(C) when C < 10 -> $0 + C;
-tohexl(C) when C < 17 -> $a + C - 10.
+	quoted:to_url(Bin, Opts).
 
 -spec x_www_form_urlencoded(binary()) -> list({binary(), binary() | true}).
 x_www_form_urlencoded(<<>>) ->
